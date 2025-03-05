b107d2e02d68a8a570a7734c4752eaa11cb5b32d
Yeakubkhan <mdyeakubkhan768@gmail.com>
2025-03-05 23:03:52 +0600

    pro

--- study/learning/views.py
+++ study/learning/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def machine(request):
-    return render(request,'learning/learning.html')
+    learn = "Django"
+    time = " tow Hours"
+    sediul = "10:30 am to 11:00 pm"
+    offer={'ln':learn,'t': time, 's': sediul ,'what': 'Bacend website development!'}
+    return render(request,'learning/learning.html',context=offer)
