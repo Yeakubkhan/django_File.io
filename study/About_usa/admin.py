4015ee99144ad7f909a775b090573bd1d6b67937
Yeakubkhan <mdyeakubkhan768@gmail.com>
2025-05-18 11:48:49 +0600

    file

--- study/About_usa/admin.py
+++ study/About_usa/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from About_usa.models import Techarc
 
 # Register your models here.
+class TecharcAdmin(admin.ModelAdmin):
+    list_display=('id','tid','tname','temail')
+
+admin.site.register(Techarc,TecharcAdmin)
